ae38777cc281585084d57a05132371fe94907106
RicoAraujo <ricardoaraujo038@gmail.com>
2024-08-21 22:23:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const nome = "Riico "
+const MeuHeroi = "O heroi de nome "
+const NoNivel = "está no nível de "
+let XP = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+console.log(MeuHeroi + (nome) + NoNivel + (XP[4]))
+
+
+let NivelPrata = [2001 <= 5000]
+let NivelOuro = [5001 <= 7000]
+let NivelPlatina = [7001 <= 8000]
+
+let NivelDeXP = "Platina"
+if (NivelDeXP === 5000) { console.log("Prata") } else if (NivelDeXP === 7000) { console.log("Ouro") } else (NivelDeXP === 8000) ; { console.log("Platina") }
